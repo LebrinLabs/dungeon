8fb1d057f44e75c1ef9457dc603eae54e731c66c
LebrinLabs <henriquelebredevin@gmail.com>
2026-06-07 15:44:52 -0300

    Nosso héroi dá seus primeiros passos pelo jogo!

--- dungeon.c
+++ dungeon.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 
+#define ALTURA 5
+#define LARGURA 9
+
 int main(void){
+
+    char mapa[ALTURA][LARGURA+1] = {
+        "#########",
+        "#.......#",
+        "#.......#",
+        "#.......#",
+        "#########",
+    };
+
     int x, y, rodando, tecla;
 
-    x = 5;
-    y = 3;
+    x = 4;
+    y = 2;
     rodando = 1;
 
     printf("Bem-vindo ao jogo de Dungeon!\n");
@@ -12,30 +24,40 @@ int main(void){
     printf("Posição inicial: (%d, %d)\n", x, y);
 
     while(rodando){
-        tecla = getchar();
-
-        switch(tecla){
-            case 'w':
-                y = y - 1;
-                break;
-            case 's':
-                y = y + 1;
-                break;
-            case 'a':
-                x = x - 1;
-                break;
-            case 'd':
-                x = x + 1;
-                break;
-            case 'q':
-                rodando = 0;
-            break;
+        for(int linha = 0; linha < ALTURA; linha++){
+            for(int coluna = 0; coluna < LARGURA; coluna++){
+                if (coluna == x && linha == y ){
+                    printf("@");
+                } else {
+                    printf("%c", mapa[linha][coluna]);
+                }
+            }
+            printf("\n");
         }
 
-        printf("Posição atual: (%d, %d)\n", x, y);
+        printf("MOVA: ");
+
+        tecla = getchar();
+
+        int novo_x, novo_y;
+
+        novo_x = x;
+        novo_y = y;
+
+        switch(tecla){
+            case 'w': novo_y = y-1; break;
+            case 's': novo_y = y+1; break;
+            case 'a': novo_x = x-1; break;
+            case 'd': novo_x = x+1; break;
+            case 'q': rodando = 0; break;
+        }
+
+        if(mapa[novo_y][novo_x] != '#'){
+            x = novo_x;
+            y = novo_y;
+        }
+
     }
-
-    printf("Obrigado por jogar!\n");
-
+    printf("Valeu pelo jogo!\n");
     return 0;
 }

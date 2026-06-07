93bbba2e3fad6ba26e1b67d852587f49ac035e0f
LebrinLabs <henriquelebredevin@gmail.com>
2026-06-07 16:09:18 -0300

    Desmenbrando a função Main, e utilizando ponteiros

--- dungeon.c
+++ dungeon.c
@@ -3,9 +3,27 @@
 #define ALTURA 5
 #define LARGURA 9
 
-int main(void){
+// recebe o mapa e os ENDERECOS de x e y, pra poder atualizar a posicao
+void mover(char mapa[][LARGURA + 1], int *x, int *y, int tecla) {
+    int novo_x = *x;
+    int novo_y = *y;
 
-    char mapa[ALTURA][LARGURA+1] = {
+    switch (tecla) {
+        case 'w': novo_y = *y - 1; break;
+        case 's': novo_y = *y + 1; break;
+        case 'a': novo_x = *x - 1; break;
+        case 'd': novo_x = *x + 1; break;
+    }
+
+    // so move se o destino nao for parede
+    if (mapa[novo_y][novo_x] != '#') {
+        *x = novo_x;
+        *y = novo_y;
+    }
+}
+
+int main(void) {
+    char mapa[ALTURA][LARGURA + 1] = {
         "#########",
         "#.......#",
         "#.......#",
@@ -13,20 +31,16 @@ int main(void){
         "#########",
     };
 
-    int x, y, rodando, tecla;
+    int x = 4;
+    int y = 2;
+    int rodando = 1;
+    int tecla;
 
-    x = 4;
-    y = 2;
-    rodando = 1;
-
-    printf("Bem-vindo ao jogo de Dungeon!\n");
-    printf("Use as teclas WASD para se mover e Q para sair\n");
-    printf("Posição inicial: (%d, %d)\n", x, y);
-
-    while(rodando){
-        for(int linha = 0; linha < ALTURA; linha++){
-            for(int coluna = 0; coluna < LARGURA; coluna++){
-                if (coluna == x && linha == y ){
+    while (rodando) {
+        // desenha o mapa com o jogador
+        for (int linha = 0; linha < ALTURA; linha++) {
+            for (int coluna = 0; coluna < LARGURA; coluna++) {
+                if (coluna == x && linha == y) {
                     printf("@");
                 } else {
                     printf("%c", mapa[linha][coluna]);
@@ -34,30 +48,17 @@ int main(void){
             }
             printf("\n");
         }
-
-        printf("MOVA: ");
+        printf("WASD move, Q sai.\n");
 
         tecla = getchar();
 
-        int novo_x, novo_y;
-
-        novo_x = x;
-        novo_y = y;
-
-        switch(tecla){
-            case 'w': novo_y = y-1; break;
-            case 's': novo_y = y+1; break;
-            case 'a': novo_x = x-1; break;
-            case 'd': novo_x = x+1; break;
-            case 'q': rodando = 0; break;
+        if (tecla == 'q') {
+            rodando = 0;
+        } else {
+            mover(mapa, &x, &y, tecla);   // passa os ENDERECOS de x e y
         }
-
-        if(mapa[novo_y][novo_x] != '#'){
-            x = novo_x;
-            y = novo_y;
-        }
-
     }
+
     printf("Valeu pelo jogo!\n");
     return 0;
 }
